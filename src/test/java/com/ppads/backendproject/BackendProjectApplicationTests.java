e99750134732c588aeb7425765be677a2f37868b
Felix Queiroz <limaqfelix@gmail.com>
2024-03-27 01:50:15 -0300

    Create Spring Boot project

---
+++ src/test/java/com/ppads/backendproject/BackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppads.backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
