955610b2b87c83115ff5fb6f711750ebb2bb621e
acceberc <rebecca-cunha@hotmail.com>
2024-03-28 22:07:09 -0300

    add users service and controller

--- src/main/java/com/ppads/backendproject/model/User.java
+++ src/main/java/com/ppads/backendproject/model/User.java
@@ -1,11 +1,11 @@
 package com.ppads.backendproject.model;
 
-import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
-@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -16,6 +16,10 @@ public class User {
 
     private String name;
 
+    private String password;
+
+    private String email;
+
     private String gender;
 
     private String birthDate;
@@ -25,4 +29,88 @@ public class User {
     private String phoneNumber;
 
     private String role;
+
+    public User(Integer id, String name, String password, String email, String gender, String birthDate, String address, String phoneNumber, String role) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- src/main/java/com/ppads/backendproject/model/UserLogin.java
+++ src/main/java/com/ppads/backendproject/model/UserLogin.java
@@ -1,13 +1,62 @@
 package com.ppads.backendproject.model;
 
-import lombok.Data;
-
-@Data
 public class UserLogin {
+
+    private Integer id;
+
+    private String name;
 
     private String email;
 
-    private String senha;
+    private String password;
 
     private String token;
+
+    public UserLogin(Integer id, String name, String email, String password, String token) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
